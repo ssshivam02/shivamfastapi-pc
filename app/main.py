f94c7b31a30d3a97abc680cf32cd379e577e826a
Shivam Sharma <ssshivamsharma02@gmail.com>
2022-08-13 15:30:02 +0530

    First commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from . import models
+import models
 from .database import engine
 from .routers import post, user, auth, misc, vote
 
